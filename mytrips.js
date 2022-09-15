54a698c3b81a97b4abee22898570c789bc8aecfd
Marcio Bueno <mabuen@live.com>
2022-09-15 09:19:45 +0200

    addtrip 1

--- mytrips.js
+++ mytrips.js
@@ -1,3 +1,6 @@
+//create a geocoder object to use geocode googlemaps feature
+var geocoder=new google.maps.Geocoder();
+
 $(function(){
     //fix map
     $("#addtripModal").on("shown.bs.modal",function(){
@@ -48,3 +51,53 @@ $("input[name='date'],input[name='date2']").datepicker({
     maxDate:"+12M",
     showWeek: true
 })
+
+var data;
+var departureLatitude;
+var departureLongitude;
+var destinationLatitude;
+var destinationLongitude;
+
+//click on create trip button
+$("#addtripForm").submit(function(event){
+    event.preventDefault();
+    data=$(this).serializeArray();
+    // console.log(data);
+    getAddTripDepartureCoordinates();
+})
+
+//define functions
+function getAddTripDepartureCoordinates(){
+    geocoder.geocode({
+        "address":document.getElementById("departure").value
+    },
+    function(results,status){
+        if(status==google.maps.GeocoderStatus.OK){
+            departureLongitude=results[0].geometry.location.lng();
+            departureLatitude=results[0].geometry.location.lat();
+            data.push({name:'departureLongitude',value:departureLongitude});
+            data.push({name:'departureLatitude',value:departureLatitude});
+            getAddTripDestinationCoordinates();
+        }else{
+            getAddTripDestinationCoordinates();
+        }
+    }
+    )
+}
+
+function getAddTripDestinationCoordinates(){
+    geocoder.geocode({
+        "address":document.getElementById("destination").value
+    },
+    function(results,status){
+        if(status==google.maps.GeocoderStatus.OK){
+            destinationLongitude=results[0].geometry.location.lng();
+            destinationLatitude=results[0].geometry.location.lat();
+            data.push({name:'destinationLongitude',value:destinationLongitude});
+            data.push({name:'destinationLatitude',value:destinationLatitude});
+            console.log(data);
+        }else{
+        }
+    }
+    )
+}
